1f8ca4cce57e2a6bafaf612abcdf79dc89fd5039
jessica kilby <jessicakilby@gmail.com>
2016-08-17 19:20:51 -0500

    last update

--- converter.js
+++ converter.js
@@ -2,46 +2,66 @@ var submitButton = document.getElementById("submit");
 var clearButton = document.getElementById("clear");
 var userInput = document.getElementById("inputNumber");
 var output = document.getElementById("output");
-var fahr = document.getElementById("fahr");
-var celc = document.getElementById("celc");
-
-// document.getElementById("button").onclick = determineConverter;
-
-submitButton.addEventListener("click", determineConverter);
-
-
-function determineConverter () {
-	fahr.addEventListener("click", toFahrenheit);
-	celc.addEventListener("click", toCelsius);
-}
-
+console.log(output);
 //T(°C) = (T(°F) - 32) × 5/9 
 function toCelsius (input) {
 	var input = userInput.value;
 	var fahrToCelc = ((input -32)*5)/9;
 	output.value = fahrToCelc;
 	console.log(output.value);
+	if (fahrToCelc>32){
+		output.style.color="red";
+	} else if (fahrToCelc<0){
+		output.style.color="blue";
+	} else {
+		output.style.color="green";
+	}
 }
-
 //T(°F) = T(°C) × 9/5 + 32
 function toFahrenheit (input) {
 	var input = userInput.value;
 	var celcToFahr = ((input *9)/5)+32;
 	output.value = celcToFahr;
 	console.log(output.value);
+	if (celcToFahr>90){
+		output.style.color="red";
+	} else if (celcToFahr<32){
+		output.style.color="blue";
+	} else {
+		output.style.color="green";
+	}
 }
 
+function determineConverter () {
+	var fahr = document.getElementById("fahr");
+	var celc = document.getElementById("celc");
+	fahr.addEventListener("checked", toFahrenheit);
+	celc.addEventListener("checked", toCelsius);
+	if (celc.checked) { 
+		toCelsius(); 
+		console.log("celsius was checked");
+		output.innerHTML += "<div>" + output.value + "</div>"
+	} else if (fahr.checked) {
+			toFahrenheit(); 
+			console.log("Fahrenheit was checked");
+			output.innerHTML += "<div>" + output.value + "</div>"
+		} else {
+			console.log("Nothing or both were checked");
+			}	
+}
 
+function enterKeyPressed(keypress){
+ if (keypress.which === 13) {
+   determineConverter();
+ }
+}
 
+function clear() {
+	document.getElementById("output").innerHTML = "";
+	document.getElementById("inputNumber").value = "";
+}
 
-// Get a reference to the button element in the DOM
-
-
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-// function determineConverter (clickEvent) {
-//    console.log("event", clickEvent);
-// }
-
-// Assign a function to be executed when the button is clicked
+submitButton.addEventListener("click", determineConverter);
+clearButton.addEventListener("click", clear);
+document.addEventListener("keypress", enterKeyPressed)
 
